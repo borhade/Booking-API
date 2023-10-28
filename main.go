cac082f8f1e1f53208a51c4eadbea302833c48af
vishal borhade <vishalborhade61@gmail.com>
2023-10-28 11:16:59 +0530

    handle panic in booking

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*type Rectangle struct {
+	height int
+		width  int
+}
+
+type MathOperation interface {
+	Add(a, b int) int
+	Area(a, b int) int
+}
+
+func (r Rectangle) Add(a, b int) int {
+	res := r.height + r.width + a + b
+	return res
+}
+func (r Rectangle) Area(a, b int) int {
+	res := r.height * r.width * a * b
+	return res
+}*/
+
+func main() {
+
+	defer fmt.Println("hii")
+	fmt.Println("hi my name is vishal")
+
+}
